7006a53c9fd3ee783aca03c00192f40c0fd164c3
chacha091 <chacha09@ewha.ac.kr>
2024-09-24 00:00:15 +0900

    [그리디 알고리즘] 9월 24일

---
+++ week6/BOJ_19539.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    vector<int> h;
+    long long total = 0, q = 0, r = 0;
+
+    for(int i=0; i<n; i++){
+        cin>>h[i];
+
+        total += h[i];
+        q += (h[i]/2);
+        r += (h[i]%2);
+    }
+
+    if(total % 3 != 0 || r > q){    //물뿌리개 사용 횟수보다 2로 나누어떨어지지 않는 나무의 개수가 더 많으면 NO
+        cout<<"NO";
+    }
+    else{
+        cout<<"YES";
+    }
+    
+    return 0;
+}
